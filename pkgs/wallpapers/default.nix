47f1c7563008b3baac258a37755d433615717c1e
kev <yqlbu@bu.edu>
2024-03-09 11:08:43 +0800

    feat(pkg): add wallpapers

---
+++ pkgs/wallpapers/default.nix
@@ -0,0 +1,21 @@
+{ stdenv, pkgs, ... }:
+
+let
+  name = "wallpapers";
+  version = "unstable-2024-03-09";
+  meta = with pkgs.lib; {
+    description = "NUR wallpapers for personal usage";
+    homepage = "https://github.com/yqlbu/nur-assets";
+    license = licenses.ofl;
+    platforms = platforms.linux;
+    mainProgram = pname;
+    maintainers = with maintainers; [ kev ];
+  };
+in
+stdenv.mkDerivation rec {
+  inherit name version meta;
+  src = ../../assets/wallpapers;
+  installPhase = ''
+    install -Dm444 * -t $out/share/${name}
+  '';
+}
