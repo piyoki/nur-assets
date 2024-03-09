3be8751e1d5fb64913a73fe07af09a7cc1235baf
kev <yqlbu@bu.edu>
2024-03-09 11:07:53 +0800

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My personal NUR assets repository";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      # lib = nixpkgs.lib;
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    in
+    (flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; config.allowUnfree = true; };
+      in
+      {
+        packages = {
+          # assets
+          wallpapers = pkgs.callPackage ./pkgs/wallpapers { };
+        };
+      }));
+}
